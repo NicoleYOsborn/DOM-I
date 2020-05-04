120e2b8196e2db9caf2a1d5bf6b813fff77bb243
Nicole Osborn <nicole.y.osborn@gmail.com>
2020-05-04 15:37:14 -0700

    completed tasks 1 and 2

--- js/index.js
+++ js/index.js
@@ -40,3 +40,57 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+var header = document.getElementsByTagName('header');
+
+header.textContent = 'DOM IS AWESOME';
+
+var button = document.querySelector('button');
+
+button.textContent = 'Get Started';
+
+var heroImg = document.getElementById('cta-img');
+
+heroImg.setAttribute('src', '/img/header-img.png');
+
+document.querySelector('nav a').textContent = 'Services';
+
+var servicesLink = document.querySelector('nav a');
+
+servicesLink.nextElementSibling.textContent = 'Product';
+
+servicesLink.nextElementSibling.nextElementSibling.textContent = 'Vision';
+
+servicesLink.nextElementSibling.nextElementSibling.nextElementSibling.textContent = 'Features';
+
+servicesLink.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent= 'About';
+
+servicesLink.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent= 'Contact';
+
+const mainKeys = Object.keys(document.querySelector('.main-content'));
+
+const mainh4 = document.querySelectorAll('.text-content h4');
+
+const subHeadings = ["Features", "About", "Services", "Product", "Vision"];
+
+mainh4.forEach((h4, idx) =>{h4.textContent = subHeadings[idx]});
+
+const mainPs = document.querySelectorAll('.text-content p');
+
+const paraText = ["Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",  "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.", "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."];
+
+mainPs.forEach((p, idx) => {p.textContent = paraText[idx]}) ;
+
+document.getElementById('middle-img').setAttribute('src', '/img/mid-page-accent.jpg');
+
+document.querySelector('.contact h4').textContent = 'Contact';
+
+ const contactInfo = document.querySelectorAll('.contact p');
+
+ const contactText = ["123 Way 456 Street Somewhere, USA", "1 (888) 888-8888", "sales@greatidea.io"];
+
+ contactInfo.forEach((p, indx) =>{
+   p.textContent = contactText[indx];
+ })
+
+ document.querySelector('footer p').textContent = "Copyright Great Idea! 2018";
